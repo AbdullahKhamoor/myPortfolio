b981b3e5624bbfdf0cb956d55ea6fa84fcb9c3c4
Abdullah <abdullah.khamoor4@gmail.com>
2025-07-19 16:39:28 +0500

    Navbar basic complete

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeroImage from "../assets/hero-image.png"
+const Hero = () => {
+  return (
+    <div>
+        <img src={HeroImage} alt="img" />
+        <h1></h1>
+    </div>
+  )
+}
+
+export default Hero
