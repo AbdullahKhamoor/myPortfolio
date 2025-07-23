4faf13ff6a09910d2567ceca358cec3d7e48e52f
Abdullah <abdullah.khamoor4@gmail.com>
2025-07-23 16:36:58 +0500

    Contact sec develope

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // import './App.css'
 import About from "./components/About"
+import Contact from "./components/Contact"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import Project from "./components/Project"
@@ -13,6 +14,7 @@ const App = () => {
       <About/>
       <Service/>
       <Project/>
+      <Contact/>
     </div>
   )
 }
