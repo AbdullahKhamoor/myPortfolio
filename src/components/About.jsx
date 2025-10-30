8dd72b202630b2614ece2cc44000c54448d5c930
Abdullah <abdullah.khamoor4@gmail.com>
2025-10-30 19:13:46 +0500

    updated code and redeployed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,152 +4,151 @@ import AOS from "aos";
 import 'aos/dist/aos.css';
 
 const About = () => {
-  useEffect(()=>{
-    AOS.init({duration: 900})
-  },[])
+    useEffect(() => {
+        AOS.init({ duration: 900 })
+    }, [])
 
+    return (
+        <div className='bg-black text-white py-20 w-full' id="about" >
+            <div className='container mx-auto px-8 md:px-16 lg:px-24'>
+                <h2 className='text-4xl font-bold text-center mb-12' >About Me</h2>
+                <div className='flex flex-col md:flex-row items-center md:space-x-12'>
+                    <img src={AboutImage} alt=""
+                        className='w-72 h-80 rounded object-cover mb-8 md:mb-0' data-aos='fade-right' />
+                    <div className='flex-1 '>
+                        <p className='hidden md:block text-lg mb-8   ' data-aos="fade-up">
+                            I am passionate full-stack developer with a focus on building modern and responsive web application
+                            with a strong foundation in both frontend and backend technologies, I strive to create seamless and efficient
+                            user experiences.
+                        </p>
+                        {/* Mobile  Responsive */}
+                        <p className='block md:hidden text-lg mb-8  ' data-aos="fade-up">
+                            I am passionate full-stack developer with a focus on building modern and responsive web application
+                            with a strong foundation in both frontend and backend technologies, I strive to create seamless and efficient
+                            user experiences.
+                        </p>
 
-  return (
-    <div className='bg-black text-white py-20 w-full' id="about" >
-        <div className='container mx-auto px-8 md:px-16 lg:px-24'>   
-            <h2 className='text-4xl font-bold text-center mb-12' >About Me</h2>
-            <div className='flex flex-col md:flex-row items-center md:space-x-12'>
-                <img src={AboutImage} alt=""
-                 className='w-72 h-80 rounded object-cover mb-8 md:mb-0' data-aos = 'fade-right' />
-                 <div className='flex-1 '>
-                <p className='hidden md:block text-lg mb-8   ' data-aos="fade-left">
-                    I am passionate full-stack developer with a focus on building modern and responsive web application
-                    with a strong foundation in both frontend and backend technologies, I strive to create seamless and efficient
-                    user experiences.
-                </p>
-                {/* Mobile  Responsive */}
-                <p className='block md:hidden text-lg mb-8  ' data-aos="fade-up">
-                    I am passionate full-stack developer with a focus on building modern and responsive web application
-                    with a strong foundation in both frontend and backend technologies, I strive to create seamless and efficient
-                    user experiences.
-                </p>
-                
-                <div className='hidden md:block space-y-4 ' data-aos="fade-left" >
-                    <div className='flex items-center'>
-                    <label htmlFor="htmlandcss" className='w-2/12'>HTML & CSS</label>
-                    <div className='grow bg-gray-800 rounded-full h-2.5'>
-                        <div className='bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded 
+                        <div className='hidden md:block space-y-4 ' data-aos="fade-up" >
+                            <div className='flex items-center'>
+                                <label htmlFor="htmlandcss" className='w-2/12'>HTML & CSS</label>
+                                <div className='grow bg-gray-800 rounded-full h-2.5'>
+                                    <div className='bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded 
                         transform transition-transform duration-300 hover:scale-105 w-10/12'>
 
-                        </div>
+                                    </div>
 
-                    </div>
-                    </div>
-                    <div className='flex items-center'>
-                    <label htmlFor="htmlandcss" className='w-2/12'>React JS </label>
-                    <div className='grow bg-gray-800 rounded-full h-2.5'>
-                        <div className='bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded 
+                                </div>
+                            </div>
+                            <div className='flex items-center'>
+                                <label htmlFor="htmlandcss" className='w-2/12'>React JS </label>
+                                <div className='grow bg-gray-800 rounded-full h-2.5'>
+                                    <div className='bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded 
                         transform transition-transform duration-300 hover:scale-105 w-11/12'>
 
-                        </div>
+                                    </div>
 
-                    </div>
-                    </div>
-                    <div className='flex items-center'>
-                    <label htmlFor="htmlandcss" className='w-2/12'>Node JS</label>
-                    <div className='grow bg-gray-800 rounded-full h-2.5'>
-                        <div className='bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded 
+                                </div>
+                            </div>
+                            <div className='flex items-center'>
+                                <label htmlFor="htmlandcss" className='w-2/12'>Node JS</label>
+                                <div className='grow bg-gray-800 rounded-full h-2.5'>
+                                    <div className='bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded 
                         transform transition-transform duration-300 hover:scale-105 w-8/12'
-                        ></div>
-                    </div>
-                    </div>
-                </div>
-{/*  mobile Animation */}
+                                    ></div>
+                                </div>
+                            </div>
+                        </div>
+                        {/*  mobile Animation */}
 
-                <div className='block md:hidden space-y-4 ' data-aos="fade-right" >
-                    <div className='flex items-center'>
-                    <label htmlFor="htmlandcss" className='w-2/12'>HTML & CSS</label>
-                    <div className='grow bg-gray-800 rounded-full h-2.5'>
-                        <div className='bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded 
+                        <div className='block md:hidden space-y-4 ' data-aos="fade-right" >
+                            <div className='flex items-center'>
+                                <label htmlFor="htmlandcss" className='w-2/12'>HTML & CSS</label>
+                                <div className='grow bg-gray-800 rounded-full h-2.5'>
+                                    <div className='bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded 
                         transform transition-transform duration-300 hover:scale-105 w-10/12'>
 
-                        </div>
+                                    </div>
 
-                    </div>
-                    </div>
-                    <div className='flex items-center'>
-                    <label htmlFor="htmlandcss" className='w-2/12'>React JS </label>
-                    <div className='grow bg-gray-800 rounded-full h-2.5'>
-                        <div className='bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded 
+                                </div>
+                            </div>
+                            <div className='flex items-center'>
+                                <label htmlFor="htmlandcss" className='w-2/12'>React JS </label>
+                                <div className='grow bg-gray-800 rounded-full h-2.5'>
+                                    <div className='bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded 
                         transform transition-transform duration-300 hover:scale-105 w-11/12'>
 
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div className='flex items-center'>
+                                <label htmlFor="htmlandcss" className='w-2/12'>Node JS</label>
+                                <div className='grow bg-gray-800 rounded-full h-2.5'>
+                                    <div className='bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded 
+                        transform transition-transform duration-300 hover:scale-105 w-8/12'
+                                    ></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='hidden md:block'>
+                            <div className='   mt-12 flex justify-between text-center   ' data-aos='fade-up'>
+                                <div>
+                                    <h3 className='text-2xl font-bold text-transparent bg-clip-text
+                         bg-gradient-to-r from-green-400 to-blue-500'>
+                                        3+
+                                    </h3>
+                                    <p>Years Experience</p>
+                                </div>
+                                <div>
+                                    <h3 className='text-2xl font-bold text-transparent bg-clip-text
+                         bg-gradient-to-r from-green-400 to-blue-500'>
+                                        10+
+                                    </h3>
+                                    <p>Projects Completed</p>
+                                </div>
+                                <div>
+                                    <h3 className='text-2xl font-bold text-transparent bg-clip-text
+                         bg-gradient-to-r from-green-400 to-blue-500'>
+                                        10+
+                                    </h3>
+                                    <p>Happy Clients</p>
+                                </div>
+                            </div>
+                        </div>
+                        {/* Mobile Animation */}
+                        <div className='block md:hidden'>
+                            <div className='   mt-12 flex justify-between text-center   ' data-aos='fade-right'>
+                                <div>
+                                    <h3 className='text-2xl font-bold text-transparent bg-clip-text
+                         bg-gradient-to-r from-green-400 to-blue-500'>
+                                        3+
+                                    </h3>
+                                    <p>Years Experience</p>
+                                </div>
+                                <div>
+                                    <h3 className='text-2xl font-bold text-transparent bg-clip-text
+                         bg-gradient-to-r from-green-400 to-blue-500'>
+                                        10+
+                                    </h3>
+                                    <p>Projects Completed</p>
+                                </div>
+                                <div>
+                                    <h3 className='text-2xl font-bold text-transparent bg-clip-text
+                         bg-gradient-to-r from-green-400 to-blue-500'>
+                                        10+
+                                    </h3>
+                                    <p>Happy Clients</p>
+                                </div>
+                            </div>
                         </div>
 
-                    </div>
-                    </div>
-                    <div className='flex items-center'>
-                    <label htmlFor="htmlandcss" className='w-2/12'>Node JS</label>
-                    <div className='grow bg-gray-800 rounded-full h-2.5'>
-                        <div className='bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded 
-                        transform transition-transform duration-300 hover:scale-105 w-8/12'
-                        ></div>
-                    </div>
+                        {/* ********  ********* */}
                     </div>
                 </div>
-                <div className='hidden md:block'>
-                <div className='   mt-12 flex justify-between text-center   ' data-aos = 'fade-left'>
-                    <div>
-                        <h3 className='text-2xl font-bold text-transparent bg-clip-text
-                         bg-gradient-to-r from-green-400 to-blue-500'>
-                            3+
-                        </h3>
-                        <p>Years Experience</p>
-                    </div>
-                    <div>
-                        <h3 className='text-2xl font-bold text-transparent bg-clip-text
-                         bg-gradient-to-r from-green-400 to-blue-500'>
-                            10+
-                        </h3>
-                        <p>Projects Completed</p>
-                    </div>
-                    <div>
-                        <h3 className='text-2xl font-bold text-transparent bg-clip-text
-                         bg-gradient-to-r from-green-400 to-blue-500'>
-                            10+
-                        </h3>
-                        <p>Happy Clients</p>
-                    </div>
-                </div>
-                </div>
-                {/* Mobile Animation */}
-                <div className='block md:hidden'>
-                <div className='   mt-12 flex justify-between text-center   ' data-aos = 'fade-right'>
-                    <div>
-                        <h3 className='text-2xl font-bold text-transparent bg-clip-text
-                         bg-gradient-to-r from-green-400 to-blue-500'>
-                            3+
-                        </h3>
-                        <p>Years Experience</p>
-                    </div>
-                    <div>
-                        <h3 className='text-2xl font-bold text-transparent bg-clip-text
-                         bg-gradient-to-r from-green-400 to-blue-500'>
-                            10+
-                        </h3>
-                        <p>Projects Completed</p>
-                    </div>
-                    <div>
-                        <h3 className='text-2xl font-bold text-transparent bg-clip-text
-                         bg-gradient-to-r from-green-400 to-blue-500'>
-                            10+
-                        </h3>
-                        <p>Happy Clients</p>
-                    </div>
-                </div>
-                </div>
-              
-                {/* ********  ********* */}
-                </div>
+
             </div>
-            
-         </div>
-    </div>
-  )
+        </div>
+    )
 }
 
 export default About
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,81 +5,81 @@ import AOS3 from "aos";
 import 'aos/dist/aos.css';
 
 const Contact = () => {
-  
-     const [aosAnimation, setAosAnimation] = useState('fade-up');
-    useEffect(()=>{
+
+  const [aosAnimation, setAosAnimation] = useState('fade-up');
+  useEffect(() => {
     const handleResize = () => {
-          if (window.innerWidth < 768) {
-            setAosAnimation('fade-right'); // Mobile
-          } else {
-            setAosAnimation('fade-left'); // Desktop
-          }
-        };
-         // Initial check and event listener
-        handleResize();
-        window.addEventListener('resize', handleResize);
-        AOS3.init({duration: 900})
-        return () => window.removeEventListener('resize', handleResize);
-    
-      },[])
+      if (window.innerWidth < 769) {
+        setAosAnimation('fade-right'); // Mobile
+      } else {
+        setAosAnimation('fade-left'); // Desktop
+      }
+    };
+    // Initial check and event listener
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    AOS3.init({ duration: 900 })
+    return () => window.removeEventListener('resize', handleResize);
+
+  }, [])
 
 
-    // AOS3.init({duration:900});
-    
+  // AOS3.init({duration:900});
+
   return (
     <div className='bg-black text-white py-20' id='contact'>
-        <div className='container mx-auto px-8 md:px-16 lg:px-24'>
-            <h2 className='text-4xl font-bold text-center mb-12'>Contact Me</h2>
-            <div className='flex flex-col md:flex-row items-center md:space-x-12 ' >
-                <div data-aos = "fade-right" className='flex-1'>
-                    <h3 className='text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r 
+      <div className='container mx-auto px-8 md:px-16 lg:px-24'>
+        <h2 className='text-4xl font-bold text-center mb-12'>Contact Me</h2>
+        <div className='flex flex-col md:flex-row items-center md:space-x-12 ' >
+          <div data-aos="fade-right" className='flex-1'>
+            <h3 className='text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r 
             from-green-400 to-blue-500 mb-4'>Let's Talk</h3>
-                    <p>I'm open to discussing web development projects or partnership opportunities.</p>
-                    <div className='mb-4 mt-8'>
-                       <FaEnvelope className='inline-block text-green-400 mr-2'></FaEnvelope> 
-                       <a href="mailto:youremail@example.com" className='hover:underline'>
-                        youremail@example.com
-                       </a>
-                    </div>
-                    <div className='mb-4 '>
-                       <FaPhone className='inline-block text-green-400 mr-2'></FaPhone>
-                       <span>+923170118530</span>
-                    </div>
-                    <div className='mb-4'>
-                       <FaMapMarkedAlt className='inline-block text-green-400 mr-2'></FaMapMarkedAlt> 
-                       <a href="mailto:youremail@example.com" className='hover:underline'>
-                        youremail@example.com
-                       </a>
-                    </div>
-                </div>
-            
-            <div data-aos = {aosAnimation} className = 'flex-1 w-full' >
-                <form className='space-y-4' >
-                    <div >
-                    <label htmlFor="name">Your Name</label>
-                    <input type="text"
-                    className='w-full p-2 rounded bg-gray-800 border-gray-600 focus:outline-none focus:border-green-400'
-                    placeholder='Enter Your Name' />
-                    </div>
-                    <div>
-                    <label htmlFor="name" className='block mb-2'>Email</label>
-                    <input type="text"
-                    className='w-full p-2 rounded bg-gray-800 border-gray-600 focus:outline-none focus:border-green-400'
-                    placeholder='Enter Your Name' />
-                    </div>
-                    <div>
-                    <label htmlFor="email" className='block mb-2'>Message</label>
-                    <textarea type="text"
-                    className='w-full p-2 rounded bg-gray-800 border-gray-600 focus:outline-none focus:border-green-400'
-                    rows="5"
-                    placeholder='Enter Your Message' />
-                    </div>
-                    <button className='bg-gradient-to-r from-green-400 to-blue-500 text-white  md:inline
-                       transform transition-transform duration-300 hover:scale-105 px-8 py-2 rounded-full'>Send </button>
-                </form>
+            <p>I'm open to discussing web development projects or partnership opportunities.</p>
+            <div className='mb-4 mt-8'>
+              <FaEnvelope className='inline-block text-green-400 mr-2'></FaEnvelope>
+              <a href="mailto:youremail@example.com" className='hover:underline'>
+                youremail@example.com
+              </a>
             </div>
+            <div className='mb-4 '>
+              <FaPhone className='inline-block text-green-400 mr-2'></FaPhone>
+              <span>+923170118530</span>
+            </div>
+            <div className='mb-4'>
+              <FaMapMarkedAlt className='inline-block text-green-400 mr-2'></FaMapMarkedAlt>
+              <a href="mailto:youremail@example.com" className='hover:underline'>
+                youremail@example.com
+              </a>
+            </div>
+          </div>
+
+          <div data-aos={aosAnimation} className='flex-1 w-full' >
+            <form className='space-y-4' >
+              <div >
+                <label htmlFor="name">Your Name</label>
+                <input type="text"
+                  className='w-full p-2 rounded bg-gray-800 border-gray-600 focus:outline-none focus:border-green-400'
+                  placeholder='Enter Your Name' />
+              </div>
+              <div>
+                <label htmlFor="name" className='block mb-2'>Email</label>
+                <input type="text"
+                  className='w-full p-2 rounded bg-gray-800 border-gray-600 focus:outline-none focus:border-green-400'
+                  placeholder='Enter Your Email' />
+              </div>
+              <div>
+                <label htmlFor="email" className='block mb-2'>Message</label>
+                <textarea type="text"
+                  className='w-full p-2 rounded bg-gray-800 border-gray-600 focus:outline-none focus:border-green-400'
+                  rows="5"
+                  placeholder='Enter Your Message' />
+              </div>
+              <button className='bg-gradient-to-r from-green-400 to-blue-500 text-white  md:inline
+                       transform transition-transform duration-300 hover:scale-105 px-8 py-2 rounded-full'>Send </button>
+            </form>
+          </div>
         </div>
-    </div>
+      </div>
     </div>
   )
 }
