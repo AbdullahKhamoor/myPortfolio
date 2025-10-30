a08682e0fcf7725a37a0e889a2bf5a5f9b7d7a59
Abdullah <abdullah.khamoor4@gmail.com>
2025-10-30 21:40:08 +0500

    updated code redeployed

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -31,44 +31,44 @@ const projects = [
   },
 ];
 const Project = () => {
- const [aosAnimation, setAosAnimation] = useState('fade-up');
-useEffect(()=>{
-const handleResize = () => {
+  const [aosAnimation, setAosAnimation] = useState('fade-up');
+  useEffect(() => {
+    const handleResize = () => {
       if (window.innerWidth < 768) {
         setAosAnimation('fade-up'); // Mobile
       } else {
         setAosAnimation('zoom-in'); // Desktop
       }
     };
-     // Initial check and event listener
+    // Initial check and event listener
     handleResize();
     window.addEventListener('resize', handleResize);
-    AOS2.init({duration: 900})
+    AOS2.init({ duration: 900 })
     return () => window.removeEventListener('resize', handleResize);
 
-  },[])
+  }, [])
 
-// AOS2.init({duration:900});
+  // AOS2.init({duration:900});
 
   return (
     <div className='bg-black text-white py-20' id="project" >
-        <div className='container mx-auto px-8 md:px-16 lg:px-24'>   
-            <h2 className='text-4xl font-bold text-center mb-12'>My Projects</h2>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
-                {projects.map((project)=> (
-                    <div key={project.id} className="bg-gray-800 p-6 rounded-lg hover:shadow-lg 
-            transform transition-transform duration-300 hover:scale-105" data-aos = {aosAnimation} >
-                    <img src={project.image} alt="" className='rounded-lg mb-4 
-              w-full h-48 object-cover' />    
-                    <h3 className="text-2xl font-bold mb-2">{project.name}</h3>
-                    <p className="text-gray-400 mb-4">{project.technologies}</p>
-                    <a href={project.github} className='inline-block bg-gradient-to-r
+      <div className='container mx-auto px-8 md:px-16 lg:px-24'>
+        <h2 className='text-4xl font-bold text-center mb-12'>My Projects</h2>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
+          {projects.map((project) => (
+            <div key={project.id} className="bg-gray-800 p-6 rounded-lg hover:shadow-lg 
+            transform transition-transform duration-300 hover:scale-105" data-aos={aosAnimation} >
+              <img src={project.image} alt="" className='rounded-lg mb-4 
+              w-full h-48 object-cover' />
+              <h3 className="text-2xl font-bold mb-2">{project.name}</h3>
+              <p className="text-gray-400 mb-4">{project.technologies}</p>
+              <a href="/" className='inline-block bg-gradient-to-r
                     from-green-400 to-blue-500 text-white px-4 py-2 rounded-full' target='_blank'>GitHub</a>
-                    </div>
-                ) )}
             </div>
+          ))}
+        </div>
+      </div>
     </div>
-    </div>        
   )
 }
 
