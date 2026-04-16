376634e5981284ff8a6783d659be660642204d11
user.name: "AbdullahKhamoor <abdullah.khamoor4@gmail.com>
2026-04-16 13:50:28 +0500

    vercel config update

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/myPortfolio/",
+  base: "/",
   plugins: [
     react(), tailwindcss()],
   theme: {
