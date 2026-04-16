1c60a72763793a135996c924e969e6504919159d
user.name: "AbdullahKhamoor <abdullah.khamoor4@gmail.com>
2026-04-16 14:13:25 +0500

    Hero Component Resum path update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
         <button
           className='bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline
             transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Contact With Me </button>
-        <a href="/myPortfolio/Resume.pdf" target="_blank"   >
+        <a href="/Resume.pdf" target="_blank"   >
           <button
             className='bg-gradient-to-r from-pink-500 to-yellow-500 text-white  md:inline
               transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>View Resume</button>
